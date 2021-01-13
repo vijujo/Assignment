7596f29b528f278318ceb618549d8969fed1263f
Viju Joseph <viju.joseph@philips.com>
2021-01-13 12:49:03 +0100

    added method to get non_completed todos only

--- TodoApp/views.py
+++ TodoApp/views.py
@@ -4,7 +4,6 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from TodoApp.models import Board, Todo
 from TodoApp.serializers import BoardSerializer, TodoSerializer, TodoUpdateSerializer
-from rest_framework import generics
 
 
 @api_view(['GET', 'POST'])
@@ -64,6 +63,15 @@ def todos_of_board(request, pk):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET'])
+def todos_uncompleted(request):
+    if request.method == 'GET':
+        todos = Todo.objects.filter(done=False)
+        serializer = TodoSerializer(todos, many=True)
+        return Response(serializer.data)
+
 
 @api_view(['GET', 'PUT', 'DELETE'])
 def todo_detail(request, pk):
