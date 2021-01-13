5db643960b08507509c13c08de7cab816fc1d5b7
Viju Joseph <viju.joseph@philips.com>
2021-01-13 08:02:30 +0100

    added serializer for todo

--- TodoApp/models.py
+++ TodoApp/models.py
@@ -1,8 +1,6 @@
 from django.db import models
 
 
-# Create your models here.
-
 class Board(models.Model):
     name = models.CharField(max_length=200)
 
@@ -12,4 +10,7 @@ class Todo(models.Model):
     done = models.BooleanField(default=False)
     created = models.DateTimeField('date created')
     updated = models.DateTimeField('date updated')
-    board = models.ForeignKey(Board, on_delete=models.CASCADE)
+    board = models.ForeignKey(Board, related_name='todos',on_delete=models.CASCADE)
+
+    def __str__(self):
+        return '%s, %s, %s, %s' % (self.title, self.done, self.created, self.updated)
--- TodoApp/serializers.py
+++ TodoApp/serializers.py
@@ -3,14 +3,33 @@ from TodoApp.models import Board, Todo
 
 
 class BoardSerializer(serializers.ModelSerializer):
+    todos = serializers.StringRelatedField(many=True)
+
     class Meta:
         model = Board
-        fields = ['id', 'name']
+        fields = ['id', 'name', 'todos']
 
     def create(self, validated_data):
         return Board.objects.create(**validated_data)
 
     def update(self, instance, validated_data):
         instance.name = validated_data.get('name', instance.name)
+        instance.save()
+        return instance
+
+
+class TodoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Todo
+        fields = ['id', 'title', 'done', 'created', 'updated', 'board']
+
+    def create(self, validated_data):
+        return Todo.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.title = validated_data.get('title', instance.title)
+        instance.done = validated_data.get('done', instance.done)
+        instance.created = validated_data.get('created', instance.created)
+        instance.updated = validated_data.get('updated', instance.updated)
         instance.save()
         return instance
