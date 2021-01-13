56a5da6cbb8c12b02c6ce49c8d419bb71740a827
Viju Joseph <viju.joseph@philips.com>
2021-01-13 11:48:55 +0100

    reverted back to api_view decorators

--- TodoApp/urls.py
+++ TodoApp/urls.py
@@ -2,8 +2,17 @@ from django.urls import path
 from TodoApp import views
 
 urlpatterns = [
-    path('api/v1/boards', views.BoardsList.as_view()),
-    path('api/v1/boards/<int:pk>/', views.BoardDetail.as_view()),
-    path('api/v1/boards/<int:pk>/todos/', views.TodosList.as_view()),
-    path('api/v1/boards/<int:pk>/todos/<int:todopk>', views.TodoDetail.as_view()),
+    #path('api/v1/boards', views.BoardsList.as_view()),
+    #path('api/v1/boards/<int:pk>/', views.BoardDetail.as_view()),
+    #path('api/v1/boards/<int:pk>/todos/', views.TodosList.as_view()),
+    #path('api/v1/boards/<int:pk>/todos/<int:todopk>', views.TodoDetail.as_view()),
+    # path('api/v1/boards/<int:pk>/todos/<int:todopk>', views.TodoDetail.as_view()),
+    # url(r'^api/v1/boards/(?P<board_pk>\d+)/todos$', views.TodosList.as_view(), name='todos_of_a_board'),
+
+    path('api/v1/boards', views.board_list),
+    path('api/v1/boards/<int:pk>/', views.board_detail),
+
+    path('api/v1/boards/<int:pk>/todos/', views.todos_of_board),
+    path('api/v1/boards/<int:pk>/todos/<int:todopk>', views.todo_detail),
+
 ]
--- TodoApp/views.py
+++ TodoApp/views.py
@@ -1,11 +1,48 @@
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
 from TodoApp.models import Board, Todo
 from TodoApp.serializers import BoardSerializer, TodoSerializer
 from rest_framework import generics
 
 
-class BoardsList(generics.ListCreateAPIView):
-    queryset = Board.objects.all()
-    serializer_class = BoardSerializer
+@api_view(['GET', 'POST'])
+def board_list(request):
+    if request.method == 'GET':
+        boards = Board.objects.all()
+        serializer = BoardSerializer(boards, many=True)
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+        serializer = BoardSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def board_detail(request, pk):
+    try:
+        board = Board.objects.get(pk=pk)
+    except Board.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = BoardSerializer(board)
+        return Response(serializer.data)
+
+    elif request.method == 'PUT':
+        serializer = BoardSerializer(board, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        board.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 
 class BoardDetail(generics.RetrieveUpdateDestroyAPIView):
@@ -13,11 +50,39 @@ class BoardDetail(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = BoardSerializer
 
 
-class TodosList(generics.ListCreateAPIView):
-    queryset = Todo.objects.all()
-    serializer_class = TodoSerializer
+@api_view(['GET', 'POST'])
+def todos_of_board(request, pk):
+    if request.method == 'GET':
+        todos = Todo.objects.filter(board=pk)
+        serializer = TodoSerializer(todos, many=True)
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+        serializer = TodoSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-class TodoDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Todo.objects.all()
-    serializer_class = TodoSerializer
+@api_view(['GET', 'PUT', 'DELETE'])
+def todo_detail(request, pk):
+    try:
+        todo = Todo.objects.get(pk=pk)
+    except Todo.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = TodoSerializer(todo)
+        return Response(serializer.data)
+
+    elif request.method == 'PUT':
+        serializer = TodoSerializer(todo, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        todo.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
