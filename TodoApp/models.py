29f7eb474118603f21d63e7cabeb6ae18d3095c4
Viju Joseph <viju.joseph@philips.com>
2021-01-13 07:43:30 +0100

    added models

--- TodoApp/models.py
+++ TodoApp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Board(models.Model):
+    name = models.CharField(max_length=200)
+
+
+class Todo(models.Model):
+    title = models.CharField(max_length=200)
+    done = models.BooleanField(default=False)
+    created = models.DateTimeField('date created')
+    updated = models.DateTimeField('date updated')
+    board = models.ForeignKey(Board, on_delete=models.CASCADE)
