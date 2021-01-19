a59c3834c95eccc48079c69d7c3168e7bc112352
Viju Joseph <viju.joseph@philips.com>
2021-01-19 08:58:28 +0100

    Made all routes consistent with trailing slashes

--- ReminderApp/urls.py
+++ ReminderApp/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from ReminderApp import views
 
 urlpatterns = [
-    path('v1/reminders', views.reminder_list),
+    path('v1/reminders/', views.reminder_list),
     path('v1/reminders/<int:pk>/', views.reminder_detail)
 ]
