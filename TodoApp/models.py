218c9350bbfed5ec3c17b47208d41e0421efe748
Viju Joseph <viju.joseph@philips.com>
2021-01-19 08:57:26 +0100

    Changed created and updated fields to have current datetime

--- TodoApp/models.py
+++ TodoApp/models.py
@@ -8,8 +8,8 @@ class Board(models.Model):
 class Todo(models.Model):
     title = models.CharField(max_length=200)
     done = models.BooleanField(default=False)
-    created = models.DateTimeField()
-    updated = models.DateTimeField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
     board = models.ForeignKey(Board, related_name='todos',on_delete=models.CASCADE)
 
     def __str__(self):
